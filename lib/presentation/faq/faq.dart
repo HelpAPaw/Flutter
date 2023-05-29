d081308371210d4c98faa0558a3be2a211df43f2
Muhammad Azri Fatihah Susanto <muhammad.azri.f.s@gmail.com>
2023-05-29 12:49:03 +0700

    refactor(faq):add-localization-in-faq

--- lib/presentation/faq/faq.dart
+++ lib/presentation/faq/faq.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class FAQSScreen extends StatelessWidget {
@@ -62,8 +63,8 @@ class _BuildFAQBody extends StatelessWidget {
 }
 
 class _BuildFAQItem extends StatelessWidget {
-  final String title;
-  final String content;
+  final String? title;
+  final String? content;
   const _BuildFAQItem({required this.title, required this.content});
 
   @override
@@ -81,8 +82,11 @@ class _BuildFAQItem extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+          const SizedBox(
+            height: 4,
+          ),
           Text(
-            content,
+            content!,
             style: const TextStyle(
               fontSize: 16,
               color: Colors.orange,
@@ -96,23 +100,19 @@ class _BuildFAQItem extends StatelessWidget {
 
 final List<Map<String, dynamic>> faqsData = [
   {
-    'title': 'How does this app work?',
-    'content':
-        'If you see a stray animal that needs help but for some reason cannot provide the help yourself you can submit a signal that marks the place and describes the situation. Other people that are nearby will receive a notification about it. Hopefully someone will react to the signal and help the animal.',
+    'title': 'faq_title_1'.tr(),
+    'content': 'faq_content1'.tr(),
   },
   {
-    'title': 'Who are the people that will help those animals?',
-    'content':
-        'Help A Paw connects a network of volunteers that care about animals - just like you.',
+    'title': 'faq_title_2'.tr(),
+    'content': 'faq_content2'.tr(),
   },
   {
-    'title': 'How does the status of the signal change?',
-    'content':
-        "When a signal is submitted it starts with status 'Help needed'. When somebody decides to answer the signal, he/she changes the status to 'Somebody on the way' so that other people know. If for example, the person arrives at the place but needs some assistance, the status can be changed back to 'Help needed'. When the animal finally receives the needed help, the signal is marked as 'Solved'. Signals are color-coded in red, orange, and green according to their status.",
+    'title': 'faq_title_3'.tr(),
+    'content': 'faq_content3'.tr(),
   },
   {
-    'title': 'Does this app track my location?',
-    'content':
-        'No, Your location is obtained and used only locally on your device. It will not be recorded on a server or used for any other purpose besides notifying you of animals in need in your area.',
-  }
+    'title': 'faq_title_4'.tr(),
+    'content': 'faq_content4'.tr(),
+  },
 ];
