9b6d6d1ee22f48cf38ed5ca3737dd3aa4fe1f380
Charles Weng <cweng8849@gmail.com>
2023-06-14 05:58:12 +0800

    upgrade flutter packages, fix widget test, update flutter code (#33)
    * upgrade flutter packages, fix widget test, update flutter code
    
    * add help a paw png to assets
    
    * add env variables for api keys
    
    Signed-off-by: Charles Weng <cweng8849@gmail.com>
    
    * add vs code local to gitignore
    
    * fix .env path
    
    ---------
    
    Signed-off-by: Charles Weng <cweng8849@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:help_a_paw/app.dart';
+import 'package:help_a_paw/utility/app_router.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(MyApp(
+      appRouter: AppRouter(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
