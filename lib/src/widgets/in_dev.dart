ea7ca33f64032a8782926a0def70a0bbf518fa94
Abdhilahi Rahim Wabwire <124460109+AbdhilahiRahimWabwire@users.noreply.github.com>
2023-07-12 16:19:23 -0600

    Add files via upload

--- lib/src/widgets/in_dev.dart
+++ lib/src/widgets/in_dev.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
 import 'package:adaptive_components/adaptive_components.dart';
 import 'package:adaptive_navigation/adaptive_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class InDev extends StatefulWidget {
   const InDev({super.key});
@@ -15,26 +12,10 @@ class InDev extends StatefulWidget {
 }
 
 class _InDevState extends State<InDev> {
-  final Uri gitHubSource = Uri(
-    host: 'github.com',
-    path: 'HelpAPaw/Flutter',
-    scheme: 'https',
-  );
-  Future<void>? _browserLaunched;
-
-  Future<void> _launchInBrowser(Uri url) async {
-    if (!await launchUrl(
-      url,
-      mode: LaunchMode.externalApplication,
-    )) {
-      return debugPrint('Launch Failed: $url');
-    }
-  }
 
   // In Development Widgets
   @override
   Widget build(BuildContext context) {
-    FutureBuilder<void>(future: _browserLaunched, builder: _launchStatus);
     return Scaffold(
       appBar: AdaptiveAppBar(
         elevation: 6,
@@ -47,46 +28,8 @@ class _InDevState extends State<InDev> {
       body: AdaptiveContainer(
         child: ListView(children: <Widget>[
           Image.asset('lib/assets/in_development.png'),
-          ButtonBar(
-            alignment: MainAxisAlignment.end,
-            children: <Widget>[
-              ElevatedButton(
-                child: const Text('GitHub Source'),
-                onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    content: const Text('Launch in Browser'),
-                    elevation: 6,
-                    icon: const Icon(Icons.open_in_browser_rounded),
-                    title: const Text('Launch Mode'),
-                    actions: <Widget>[
-                      ElevatedButton(
-                        child: const Text('Cancel'),
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                      ),
-                      ElevatedButton(
-                        child: const Text('OK'),
-                        onPressed: () => setState(() {
-                          Navigator.pop(context, 'OK');
-                          _browserLaunched = _launchInBrowser(gitHubSource);
-                        }),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          )
         ]),
       ),
     );
-  }
-
-  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
-    if (snapshot.hasError) {
-      throw Exception('Error: ${snapshot.error}');
-    } else {
-      throw Exception('Launched: $snapshot');
-    }
   }
 }
--- lib/src/widgets/licenses_page.dart
+++ lib/src/widgets/licenses_page.dart
@@ -2,7 +2,6 @@ import 'package:adaptive_components/adaptive_components.dart';
 import 'package:adaptive_navigation/adaptive_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class LicensesPage extends StatefulWidget {
   const LicensesPage({super.key});
@@ -13,31 +12,9 @@ class LicensesPage extends StatefulWidget {
 }
 
 class _LicensesPageState extends State<LicensesPage> {
-  final Uri gitHubSource = Uri(
-    host: 'github.com',
-    path: 'HelpAPaw/Flutter',
-    scheme: 'https',
-  );
-  final Uri ourSite = Uri(
-    host: 'www.helpapaw.org',
-    path: 'index_en.html',
-    scheme: 'https',
-  );
-  Future<void>? _browserLaunched;
-
-  Future<void> _launchInBrowser(Uri url) async {
-    if (!await launchUrl(
-      url,
-      mode: LaunchMode.externalApplication,
-    )) {
-      return debugPrint('Launch Failed: $url');
-    }
-  }
-
   // Licenses Widgets
   @override
   Widget build(BuildContext context) {
-    FutureBuilder<void>(future: _browserLaunched, builder: _launchStatus);
     return Scaffold(
       appBar: AdaptiveAppBar(
         elevation: 6,
@@ -48,77 +25,14 @@ class _LicensesPageState extends State<LicensesPage> {
         title: const Text('Licenses'),
       ),
       body: AdaptiveContainer(
-        child: ListView(children: <Widget>[
-          const AboutDialog(
+        child: ListView(children: const <Widget>[
+          AboutDialog(
             applicationName: 'Help a Paw',
             applicationLegalese: 'BSD 3-Clause "New" or "Revised" License',
             applicationVersion: '0.2.0',
           ),
-          ButtonBar(
-            alignment: MainAxisAlignment.end,
-            children: <Widget>[
-              ElevatedButton(
-                child: const Text('GitHub Source'),
-                onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    content: const Text('Launch in Browser'),
-                    elevation: 6,
-                    icon: const Icon(Icons.open_in_browser_rounded),
-                    title: const Text('Launch Mode'),
-                    actions: <Widget>[
-                      ElevatedButton(
-                        child: const Text('Cancel'),
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                      ),
-                      ElevatedButton(
-                        child: const Text('OK'),
-                        onPressed: () => setState(() {
-                          Navigator.pop(context, 'OK');
-                          _browserLaunched = _launchInBrowser(gitHubSource);
-                        }),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                child: const Text('Our Site'),
-                onPressed: () => showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    content: const Text('Launch in Browser'),
-                    elevation: 6,
-                    icon: const Icon(Icons.open_in_browser_rounded),
-                    title: const Text('Launch Mode'),
-                    actions: <Widget>[
-                      ElevatedButton(
-                        child: const Text('Cancel'),
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                      ),
-                      ElevatedButton(
-                        child: const Text('OK'),
-                        onPressed: () => setState(() {
-                          Navigator.pop(context, 'OK');
-                          _browserLaunched = _launchInBrowser(ourSite);
-                        }),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
         ]),
       ),
     );
-  }
-
-  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
-    if (snapshot.hasError) {
-      throw Exception('Error: ${snapshot.error}');
-    } else {
-      throw Exception('Launched: $snapshot');
-    }
   }
 }
