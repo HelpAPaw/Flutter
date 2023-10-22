bcb09ea7784e85c726ad9bac83e9b1f4960209b8
Abdhilahi Rahim Wabwire <124460109+AbdhilahiRahimWabwire@users.noreply.github.com>
2023-10-22 13:44:41 -0600

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDX000zOUB-F2qL4Wcf-pas3q3ThyHAOVc")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
