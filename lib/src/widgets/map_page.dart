f086a9028def03d8350a204ff2abd6da2181fae9
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-11-03 21:22:59 +0200

    [WIP] Get signals from Firestore and show them on the map

--- lib/src/widgets/map_page.dart
+++ lib/src/widgets/map_page.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:adaptive_components/adaptive_components.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -13,26 +12,60 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  final Completer<GoogleMapController> _mapScreenController =
-      Completer<GoogleMapController>();
-  static const CameraPosition _initialMapScreenPosition = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  final Stream<QuerySnapshot> _signalsStream =
+  FirebaseFirestore.instance.collection('signals').snapshots();
+  final LatLng _center = const LatLng(-33.86, 151.20);
+  late GoogleMapController _mapController;
 
   // Map Page Widgets
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AdaptiveContainer(
-        child: GoogleMap(
-          initialCameraPosition: _initialMapScreenPosition,
-          onMapCreated: (GoogleMapController controller) {
-            _mapScreenController.complete(controller);
-          },
-        ),
-      ),
+    return StreamBuilder<QuerySnapshot>(
+        stream: _signalsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          Set<Marker> signalMarkers = {};
+
+          if (snapshot.hasError) {
+            //TODO
+            // return const Text('Something went wrong');
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            //TODO
+            // return const Text("Loading");
+          } else {
+            var signals = snapshot.data!.docs;
+            signalMarkers = signals.map<Marker>((signalDocument) {
+              Map<String, dynamic> data = signalDocument.data()! as Map<String, dynamic>;
+              GeoPoint location = data['location'];
+              return Marker(
+                markerId: MarkerId(signalDocument.id),
+                position: LatLng(location.latitude, location.longitude),
+                infoWindow: InfoWindow(
+                  title: data['title'],
+                  snippet: data['description'],
+                ),
+              );
+            }).toSet();
+          }
+
+          return Scaffold(
+            body: AdaptiveContainer(
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    bearing: 0.0,
+                    target: _center,
+                    tilt: 0.0,
+                    zoom: 11.0
+                ),
+                onMapCreated: (GoogleMapController controller) {
+                  _mapController = controller;
+                },
+                zoomControlsEnabled: false,
+                myLocationEnabled: true,
+                markers: signalMarkers,
+              ),
+            ),
+          );
+        }
     );
   }
 }
